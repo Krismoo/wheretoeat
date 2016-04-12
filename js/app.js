784781f4c70c87cb575c0229738ff4eea2041d6e
Simon Rissi <simon.rissi@students.fhnw.ch>
2016-04-12 14:52:38 +0200

    Marker in Map added

--- js/app.js
+++ js/app.js
@@ -1,34 +1,45 @@
-function getPosition() {
-	var readPosition = function(position) {
-		console.log(position);
-		getMap(position);
-	}
-
-	return window.navigator.geolocation.getCurrentPosition(readPosition);
-}
-
-function getMap(position) {
-	var lat = (position ? position.coords.latitude : 47.5);
-	var lon = (position ? position.coords.longitude : 8.2);
-	var here = new google.maps.LatLng(lat, lon);
-
-	var mapOptions = { center: here, zoom: 15 };
-	var map = new google.maps.Map(document.getElementById("where"), mapOptions);
-
-    searchPlaces(map,here);
-
-    return map;
-}
-
-function searchPlaces(map,here){
-    var placesSearchOptions = {location: here, radius: 300, types: ['restaurant'], keyword: 'Pizza'};
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(placesSearchOptions, function(results,status){
-        $.each(results,function(k,place){
-            $("#restaurantTable").append("<tr class='restaurant'><td><img src="+place.icon+"  style=width:30px;height:30px;></td><td style=width:50px; class='number'>"+(k+1)+"</td><td>"+ place.name+"</td></tr>");
-        });
-    });
-}
 $(document).ready(function() {
-	getPosition();
+	return window.navigator.geolocation.getCurrentPosition(function(position) {
+		console.log(position);
+		
+		//create Map
+		var here = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		var mapOptions = { 
+			center: here, 
+			zoom: 15 
+		};
+		var map = new google.maps.Map(document.getElementById("where"), mapOptions);
+
+		//mark own location
+		var iconUrl = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|00FF00|ffffff';
+		var markerOptions = {
+			map: map, 
+			position: here, 
+			icon: iconUrl
+		};
+		new google.maps.Marker(markerOptions);
+		
+		//find Places
+		var placesSearchOptions = {
+			location: here, 
+			radius: 300, 
+			types: ['restaurant'], 
+			keyword: 'Pizza'
+		};
+		var service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(placesSearchOptions, function(results,status){
+			$.each(results,function(k,place){
+				$("#restaurantTable").append("<tr class='restaurant'><td><img src="+place.icon+"  style=width:30px;height:30px;></td><td style=width:50px; class='number'>"+(k+1)+"</td><td>"+ place.name+"</td></tr>");
+				
+				var iconUrl = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + (k+1) + '|FF0000|ffffff';
+				var markerOptions = {
+					map: map, 
+					position: place.geometry.location, 
+					icon: iconUrl
+				};
+				new google.maps.Marker(markerOptions);
+			});
+		});
+		
+	});
 });
