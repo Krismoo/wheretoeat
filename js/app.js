0986e5eb66e4b1d8d7202e59e7ed3ec7a35d18a0
Nicolas Novak <nicolas.novak@students.fhnw.ch>
2016-04-12 13:29:08 +0200

    Added gitignore + functionality of What

--- js/app.js
+++ js/app.js
@@ -13,9 +13,28 @@ function getMap(position) {
 	var here = new google.maps.LatLng(lat, lon);
 	
 	var mapOptions = { center: here, zoom: 15 };
-	
 	var map = new google.maps.Map(document.getElementById("where"), mapOptions);
-	return map;
+
+
+
+
+
+
+
+    var placesSearchOptions = {location: here, radius: 300, types: ['restaurant'], keyword: 'Pizza'};
+
+    var service = new google.maps.places.PlacesService(map);
+
+    service.nearbySearch(placesSearchOptions, function(results,status){
+       // ...
+        $.each(results,function(k,v){
+            $("#restaurantList").append("<li>"+v.name+"</li>");
+        });
+    });
+
+
+
+    return map;
 }
 
 $(document).ready(function() {
