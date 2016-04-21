a6e65ef43a2b9f777b184345acfb1945644a7ab5
Simon Rissi <simon.rissi@students.fhnw.ch>
2016-04-21 14:54:45 +0200

    multiple load and customized search

--- js/app.js
+++ js/app.js
@@ -1,49 +1,59 @@
+var here = "";
 $(document).ready(function(){
-		$('body').on('click', 'button', function(event) {
-			event.preventDefault();
-			alert("dfnhjztzver");
+	$('body').on('click', 'button', function(event) {
+		event.preventDefault();
 
-			return window.navigator.geolocation.getCurrentPosition(function(position) {
+		if(here) {
+			search(here);
+		} else {
+			window.navigator.geolocation.getCurrentPosition(function(position) {
 				console.log(position);
-
-				//create Map
-				var here = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-				var mapOptions = {
-					center: here,
-					zoom: 15
-				};
-				var map = new google.maps.Map(document.getElementById("where"), mapOptions);
-
-				//mark own location
-				var iconUrl = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|00FF00|ffffff';
-				var markerOptions = {
-					map: map,
-					position: here,
-					icon: iconUrl
-				};
-				new google.maps.Marker(markerOptions);
-
-				//find Places
-				var placesSearchOptions = {
-					location: here,
-					radius: 300,
-					types: ['restaurant'],
-					keyword: $("#radioForm input[name=Menu]:checked").val()	
-				};
-				var service = new google.maps.places.PlacesService(map);
-				service.nearbySearch(placesSearchOptions, function(results,status){
-					$.each(results,function(k,place){
-						$("#restaurantTable").append("<tr class='restaurant'><td><img src="+place.icon+"  style=width:30px;height:30px;></td><td style=width:50px; class='number'>"+(k+1)+"</td><td>"+ place.name+"</td></tr>");
-
-						var iconUrl = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + (k+1) + '|FF0000|ffffff';
-						var markerOptions = {
-							map: map,
-							position: place.geometry.location,
-							icon: iconUrl
-						};
-						new google.maps.Marker(markerOptions);
-					});
-				});
+				here = position;
+				search(here);
 			});
+		}
 	});
 });
+
+function search(position) {
+	//create Map
+	var here = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	var mapOptions = {
+		center: here,
+		zoom: 15
+	};
+	var map = new google.maps.Map(document.getElementById("where"), mapOptions);
+
+	//mark own location
+	var iconUrl = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|00FF00|ffffff';
+	var markerOptions = {
+		map: map,
+		position: here,
+		icon: iconUrl
+	};
+	new google.maps.Marker(markerOptions);
+
+	//find Places
+	var placesSearchOptions = {
+		location: here,
+		radius: 3000,
+		types: ['restaurant'],
+		keyword: $("#radioForm input[name=Menu]:checked").val()	
+	};
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(placesSearchOptions, function(results,status){
+		$("#restaurantTable").empty();
+		$.each(results,function(k,place){
+			$("#restaurantTable").append("<tr class='restaurant'><td><img src="+place.icon+"  style=width:30px;height:30px;></td><td style=width:50px; class='number'>"+(k+1)+"</td><td>"+ place.name+"</td></tr>");
+
+			var iconUrl = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + (k+1) + '|FF0000|ffffff';
+			var markerOptions = {
+				map: map,
+				position: place.geometry.location,
+				icon: iconUrl
+			};
+			new google.maps.Marker(markerOptions);
+		});
+		$("#whotabber").tab('show');
+	});
+}
